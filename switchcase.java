589ec0a121d1e230edeed1d5816688b1c5a5cb8b
Yashreddy1 <reddyrock170@gmail.com>
2020-06-06 21:36:24 +0530

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,48 @@
+package switchcase;
+import java.util.*;
+/**
+ *
+ * @author YASH REDDY
+ */
+public class Switchcase {
+
+    public static void main(String[] args) {
+               
+        Scanner sc=new Scanner(System.in);
+        int a;
+        System.out.println("choose any number from 1-4");
+        a= sc.nextInt();
+        switch(a){
+            case 1:{
+                System.out.println("u have choosen the first case");
+                System.out.println("good work");
+                break;
+            }
+            case 2:{
+                System.out.println("u have choosen second case");
+                System.out.println("good work");
+                break;
+            }
+            case 3:{
+                System.out.println("u have choosen third case");
+                System.out.println("good work");
+                break;
+            }
+            case 4:{
+                System.out.println("u have choosen fouth case");
+                System.out.println("good work");
+                break;
+            }
+            default:{
+                System.out.println("error!! choosen number not in range ");
+                System.out.println("bad work");
+                break;
+            }
+       
+        }
+        
+     
+        
+    
+    }
+}

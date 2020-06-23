8c2d8a5c451ee0c8aaa5a88963fdfa32547294a4
Yashreddy1 <reddyrock170@gmail.com>
2020-06-23 12:46:04 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,37 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package prime;
 import java.util.*;
-/**
- *
- * @author YASH REDDY
- */
+
 public class Prime {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        boolean flag=false;
         Scanner sc=new Scanner(System.in);
         System.out.println("enter a number to check wehter its prime or not:");
         int a=sc.nextInt();
-        boolean prime = true;
         
         for (int i=2;i<a;i++){
         if(a%i==0){
             System.out.println(a+" is not prime");
+            flag=true;
             break;
         }
-        else{
+        
+             
+        }
+        if(flag==false)
             System.out.println(a+" is prime");
-            break;
+        
         }
     }
-
-    
-    }
-}
-
